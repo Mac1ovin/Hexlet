60ba018bce515fc3fbb3df8d0962dbdadcd10055
Mac1ovin <Vasyalena777@gmail.com>
2018-08-23 16:26:35 +0300

    add folder 34

---
+++ Hexlet34/main.js
@@ -0,0 +1,15 @@
+import assert from "assert";
+import factorial from "./implementations";
+
+// Example
+// assert.equal(factorial(2), 2);
+// assert.equal(factorial(3), 6);
+
+// BEGIN (write your solution here)
+assert(factorial(0), 1);
+assert.equal(factorial(1), 1);
+assert.equal(factorial(4), 24);
+assert(factorial(0), 0);
+assert(factorial(1), 1);
+assert(factorial(5), 120);
+// END
